8078ea600042c0f8cc9a6e72faba1e0ded8ab994
vakhno <vakhnohere@gmail.com>
2020-11-02 03:22:10 +0200

    Added svg component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react'
 import EnterType from '../EnterType/EnterType'
 import { CloseContext } from '../context/CloseContext'
+import LeftArrow from '../activeIcons/LeftArrow'
+
 import './Form.sass'
 
 function Form() {
@@ -8,8 +10,8 @@ function Form() {
 
 	return (
 		<>
-			<div className="app__container">
-				{startPage ? '' : <img src="img/left-arrow.svg" alt="close-btn" className="app__close-button" onClick={() => closeCurrentPage()}></img>}
+			<div className="app__container absolute-center">
+				{startPage ? '' : <LeftArrow className="app__close-button" onClick={() => closeCurrentPage()} />}
 				<div className="app__content-left">
 					<img src="img/img-2.svg" alt="" className="app__main-image absolute-center" />
 				</div>
